5b032ff0ef9481ada6db2edcd58455aa58551249
SportWizard <45925659+SportWizard@users.noreply.github.com>
2022-11-22 16:43:08 -0500

    Add files via upload

--- search.js
+++ search.js
@@ -6,7 +6,8 @@ function search() {
     //send request
     $.post(url + "?data=" + JSON.stringify({
             "countryName": country,
-            "action": "information"}),
+            "action": "information",
+            "type": "info"}),
         store);
 }
 
@@ -14,7 +15,7 @@ function store(data) {
     var response = JSON.parse(data);
     console.log(response);
     //store info
-    var dataStore = response["name"] + "," + response["image"];
+    var dataStore = response["name"] + "|" + response["image"] + "|" + response["capital"] + "|" + response["language"] + "|" + response["population"] + "|" + response["export"] + "|" + response["import"] + "|" + response["funFact"];
     //store the data so it can be shared across html
     sessionStorage.setItem("dataStore", dataStore);
 
@@ -24,20 +25,46 @@ function store(data) {
 
 function info() {
     //get stored data that is shared and split it into an array
-    var response = sessionStorage.getItem("dataStore").split(",");
+    var response = sessionStorage.getItem("dataStore").split("|");
     //output the info
     var countryName = document.createElement("h3");
 
-    $(countryName).attr("id", "countryName");
+    $(countryName).attr("class", "countryName");
     $("#box").append(countryName);
 
-    $("#countryName").text("Country: " + response[0]);
-    $("#countryName").css({"text-align": "center"});
+    $(".countryName").text("Country: " + response[0]);
+    $(".countryName").css({"text-align": "center"});
 
+    //create img element for country flag
     var flag = document.createElement("img");
 
-    $(flag).attr("id", "flag");
+    $(flag).attr("class", "flag");
     $("#box").append(flag);
 
-    $("#flag").attr("src", response[1]);
+    $(".flag").attr("src", response[1]);
+    $(".flag").css({"border": "solid black"});
+
+    //create a box for the information
+    var information = document.createElement("div");
+
+    $(information).attr("class", "information");
+    $("#box").append(information);
+
+    //subtitle for each information
+    subtitle = ["Capital: ", "Language: ", "Population: ", "Export: ", "Import: "];
+
+    //create p block
+    for (var x = 0; x < subtitle.length; x++) {
+        var msg = document.createElement("p");
+
+        $(msg).attr("id", "msg" + x);
+        $(information).append(msg);
+    }
+
+    //add information
+    for (var y = 0; y < subtitle.length; y++) {
+        $("#msg" + y).text(subtitle[y] + response[y+2]);
+    }
+
+    $(".information").css({"float": "right", "width": "570px"});
 }
--- server.js
+++ server.js
@@ -17,31 +17,22 @@ app.post('/post', (req, res) => {
     if (data["action"] == "information") {
         switch (data["countryName"]) {
             case "Russia":
-                Russia();
-                break;
-            case "Italy":
-                Italy();
+                Russia(data["type"]);
                 break;
             case "Germany":
-                Germany();
+                Germany(data["type"]);
                 break;
-            case "Equatorial Guinea":
-                EqutorialGuinea();
+            case "Argentina":
+                Argentina(data["type"]);
                 break;
-            case "Latvia":
-                Latvia();
+            case "Australia":
+                Australia(data["type"]);
                 break;
             case "South Africa":
-                SouthAfrica();
+                SouthAfrica(data["type"]);
                 break;
             case "Iran":
-                Iran();
-                break;
-            case "USA":
-                USA();
-                break;
-            case "Canada":
-                Canada();
+                Iran(data["type"]);
                 break;
         }
     }
@@ -50,65 +41,123 @@ app.post('/post', (req, res) => {
 }).listen(port);
 console.log("server is running! (listening on port " + port + ")");
 
-function Russia() {
-    jsontext = JSON.stringify({
-        "name": "Russia",
-        "image": "images/info/Russia.jpg"
-    });
+function Russia(type) {
+    //check which type of information is need to responds/output
+    if (type == "info") {
+        jsontext = JSON.stringify({
+            "name": "Russia",
+            "image": "images/info/Russia.jpg",
+            "capital": "Moscow",
+            "language": "Russian",
+            "population": "About 143 million people",
+            "export": "Goods of energy such as oil, gas, petroleum and coal",
+            "import": "Machinery, equipment, vehicles, consumer goos, food and chemical products",
+            "funFact": "Russia has the longest railway,Largest country in the world,Has one of the busiest metros,Tetris was made in Russia,Have the codest village in the world,20% of Earth's trees are in Russia,Russia has 11 time zones,Russia is one of the first countries to produce vodka"
+        });
+    }
+    else if (type == "trade") {
+        jsontext = JSON.stringify({
+
+        });
+    }
 }
 
-function Italy() {
-    jsontext = JSON.stringify({
-        "name": "Italy",
-        "image": "images/info/Italy.jpg"
-    });
+function Germany(type) {
+    if (type == "info") {
+        jsontext = JSON.stringify({
+            "name": "Germany",
+            "image": "images/info/Germany.jpg",
+            "captial": "Berlin",
+            "language": "German",
+            "population": "83 million people",
+            "export": "Pharmaceutical products, motor cars, motor caravans, engines and other vehicle parts",
+            "import": "Motor vehicles, industrial, electrical machinery, oil, mineral fuels, Pharmaceuticals, computer equipment and chemical products",
+            "funFact": "Most popular surname is \"Muller\",Beer is a food in Bavaria,Have 1000 varieties of sausages,Gummy bears was inbented in Germany,Germany has the world largest cathedral,Attempting to escape jail is legal in Germany,University is free, largest train station in Europe,Drinking age is 16, has over 400 zoo's"
+        });
+    }
+    else if (type == "trade") {
+        jsontext = JSON.stringify({
+            
+        });
+    }
 }
 
-function Germany() {
-    jsontext = JSON.stringify({
-        "name": "Germany",
-        "image": "images/info/Germany.jpg"
-    });
+function Argentina(type) {
+    if (type == "info") {
+        jsontext = JSON.stringify({
+            "name": "Argentina",
+            "image": "",
+            "capital": "",
+            "language": "",
+            "population": "",
+            "export": "",
+            "import": "",
+            "funcFact": ""
+        });
+    }
+    else if (type == "trade") {
+        jsontext = JSON.stringify({
+
+        });
+    }
 }
 
-function EqutorialGuinea() {
-    jsontext = JSON.stringify({
-        "name": "Equtorial Guinea",
-        "image": "images/info/EqutorialGuinea.jpg"
-    });
+function Australia(type) {
+    if (type == "info") {
+        jsontext = JSON.stringify({
+            "name": "Australia",
+            "image": "",
+            "capital": "",
+            "language": "",
+            "population": "",
+            "export": "",
+            "import": "",
+            "funcFact": ""
+        });
+    }
+    else if (type == "trade") {
+        jsontext = JSON.stringify({
+
+        });
+    }
 }
 
-function Latvia() {
-    jsontext = JSON.stringify({
-        "name": "Latvia",
-        "image": "images/info/Latvia.jpg"
-    });
+function SouthAfrica(type) {
+    if (type == "info") {
+        jsontext = JSON.stringify({
+            "name": "South Africa",
+            "image": "images/info/SouthAfrica.jpg",
+            "capital": "",
+            "language": "11 Official languages",
+            "population": "",
+            "export": "Corn, precious metals, stones including diamonds, gold, platinum, machinery, chemical, vegetables and cars",
+            "import": "Petroleum, motor vehicles part and accessories, cars and broadcasting equipment",
+            "funfact": "Has the longest continuous wine route on Earth,Largest meat producer in Africa,Only country with three capital cities,World's first heart transplant was completed in Cape Town, Only country in the world where two Nobel prize winners lived on the same street"
+        });
+    }
+    else if (type == "trade") {
+        jsontext = JSON.stringify({
+            
+        });
+    }
 }
 
-function SouthAfrica() {
-    jsontext = JSON.stringify({
-        "name": "South Africa",
-        "image": "images/info/SouthAfrica.jpg"
-    });
-}
-
-function Iran() {
-    jsontext = JSON.stringify({
-        "name": "Iran",
-        "image": "images/info/Iran.jpg"
-    });
-}
-
-function USA() {
-    jsontext = JSON.stringify({
-        "name": "USA",
-        "image": "images/info/USA.jpg"
-    });
-}
-
-function Canada() {
-    jsontext = JSON.stringify({
-        "name": "Canada",
-        "image": "images/info/Canada.jpg"
-    });
+function Iran(type) {
+    if (type == "info") {
+        jsontext = JSON.stringify({
+            "name": "Iran",
+            "image": "images/info/Iran.jpg",
+            "capital": "",
+            "language": "",
+            "population": "",
+            "export": "oil, natural gas, chemicals, plastics, fruits, ceramic products and metals",
+            "import": "non-electrical machinery, steel, iron, chemicals, transport vehicles, plastics, animal, vegetable fats, oils, cleavage products",
+            "funfact": "The weekend in Iran is Friday,Thumbs-up in Iram is bad,Blowing your nose in public is rude,Majority of students in university are females,Iranians love tea"
+        });
+    }
+    else if (type == "trade") {
+        jsontext = JSON.stringify({
+            
+        });
+    }
 }
